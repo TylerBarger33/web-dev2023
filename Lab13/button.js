91a67fd3f69fc2326abc72136fc87fa2fbda0c33
TylerBarger33 <47096087+TylerBarger33@users.noreply.github.com>
2023-04-14 20:17:59 -0400

    lab13
    lab 13 index and js files

---
+++ Lab13/button.js
@@ -0,0 +1,14 @@
+function changeStyle() {
+    document.getElementById("pressingButton").style.fontSize = "32px"; 
+    document.getElementById("pressingButton").style.color = "black";
+    document.getElementById("pressingButton").style.backgroundColor = "orange"; 
+    document.getElementById("pressingButton").style.borderBlockStyle = "solid";
+    document.getElementById("pressingButton").style.borderBlockColor = "red";
+    document.getElementById("pressingButton").style.borderBlockWidth = "10px";
+    document.getElementById("pressingButton").style.padding = "10px";
+    document.getElementById("pressingButton").textContent = "There and Back Again, a Tale by Bilbo Baggins."
+
+}
+function readBooks(){
+   window.open('https://gosafir.com/mag/wp-content/uploads/2019/12/Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010.pdf', '_blank')
+}
